032a63340edbe81ede3b0057e1b0b3b692afedfd
Hendrik <83767323+mc-thulu@users.noreply.github.com>
2021-06-07 21:14:37 +0200

    small changes to comments

--- examples/instances.cpp
+++ examples/instances.cpp
@@ -1,16 +1,19 @@
 #include <dmsc/visuals.hpp>
 
 int main() {
-    // 1. create a new instance
     dmsc::Instance instance;
-    dmsc::StateVector sv;
-    sv.height_perigee = 200.f;         // height above the central mass
+
+    // 1. create satellites
+    dmsc::StateVector sv; // represents a satellite
+    sv.height_perigee = 200.f;
     instance.satellites.push_back(sv); // satellite 0
     sv.inclination = dmsc::rad(45.f);
-    instance.satellites.push_back(sv);          // satellite 1
-    instance.edges.push_back(dmsc::Edge(0, 1)); // schedule communication between sat 0 and sat 1
+    instance.satellites.push_back(sv); // satellite 1
 
-    // 2. visualize instance
+    // 2. schedule communications between satellites
+    instance.edges.push_back(dmsc::Edge(0, 1)); // communication between sat 0 and sat 1
+
+    // 3. visualize instance
     dmsc::visualizeInstance(instance);
 
     return 0;
--- include/dmsc/timeline.hpp
+++ include/dmsc/timeline.hpp
@@ -91,13 +91,11 @@ class Timeline {
         }
 
         float t_next = nextTimeWithEvent(event.t_begin, false);
+        // is there an event that overlaps with the new one?
         if (t_next != -1.f && t_next <= event.t_end) {
-            // there is another event that is currently active or will be active soon
-            // does these events overlap? -> invalid!
             return false;
         }
 
-        // no problems found -> try to insert it ...
         return events.insert(event).second;
     }
 
@@ -114,8 +112,8 @@ class Timeline {
     /**
      * @brief Return the time when the next event will become active. If an event is active at the given time, the given
      * time will be returned.
-     * If there is no event after the given time and loops are allowed, we will look for active events from the
-     * beginning. The resulting time will be less than the given time.
+     * If loops are allowed and there is no event that is currently active or will become active after the given time,
+     * we will look for active events from the beginning. The resulting time will be less than the given time.
      *
      * If no valid time is found, -1 is returned.
      */
@@ -125,11 +123,11 @@ class Timeline {
         }
 
         TimelineEvent<PayloadData> tmp(t, t);
-        auto e = events.lower_bound(tmp); // the first element that is NOT less than tmp (see "<"" of TimelineEvent)
+        auto e = events.lower_bound(tmp); // the first event that is NOT less than tmp (see "<" of TimelineEvent)
         if (e != events.end()) {
-            if (e->t_begin <= t) { // next event is currently active
+            if (e->t_begin <= t) { // event is currently active
                 return t;
-            } else { // next event is ahead
+            } else { // event will become active in the future
                 return e->t_begin;
             }
         } else if (allow_loop) { // no element found -> restart at the beginning?
@@ -137,17 +135,17 @@ class Timeline {
         }
 
         // TODO error handling
-        return -1.f; // no element NOT less than tmp AND no restart allowed
+        return -1.f;
     }
 
     /**
-     * @brief Return the first event that will become active (starting from the given time). If an event is already
-     * active at the given time, this event will be returned.
+     * @brief Return the first event that will become active (starting from the given time). If an event is active at
+     * the given time, this event will be returned.
      *
-     * If there is no event active after the given time and loops are allowed, we will look for events from the
-     * beginning. The resulting event will start before the given time.
+     * If loops are allowed and there is no event that is currently active or will become active after the given time,
+     * we will look for active events from the beginning. The resulting event will begin before the given time.
      *
-     * If no valid time is found, an invalid event is returned.
+     * If no such event is found, an invalid event is returned.
      */
     TimelineEvent<PayloadData> prevailingEvent(const float t, const bool allow_loop = false) const {
         if (events.size() == 0) {
@@ -155,23 +153,23 @@ class Timeline {
         }
 
         TimelineEvent<PayloadData> tmp(t, t);
-        auto e = events.lower_bound(tmp); // the first element that is NOT less than tmp (see "<"" of TimelineEvent)
+        auto e = events.lower_bound(tmp); // the first element that is NOT less than tmp (see "<" of TimelineEvent)
         if (e != events.end()) {
             return *e;
         } else if (allow_loop) { // no element found -> restart at the beginning?
             return *events.begin();
         }
 
-        return TimelineEvent<PayloadData>(TIMELINE_ERR, -1.f); // no element NOT less than tmp AND no restart allowed
+        return TimelineEvent<PayloadData>(TIMELINE_ERR, -1.f);
     }
 
     /**
      * @brief Return the last event that ended before the given time. The returned event won't be active.
      *
-     * If there is no event before the given time and loops are allowed, then we will look for events from the
-     * end. The resulting event will end after the given time.
+     * If loops are allowed and there is no event that ended before the given time, the last event in this timeline will
+     * be returned. The resulting event will end after the given time.
      *
-     * If no valid time is found, an invalid event is returned.
+     * If no such event is found, an invalid event is returned.
      */
     TimelineEvent<PayloadData> previousEvent(const float t, const bool allow_loop = false) const {
         if (events.size() == 0) {
@@ -179,14 +177,14 @@ class Timeline {
         }
 
         TimelineEvent<PayloadData> tmp(t, t);
-        auto e = events.lower_bound(tmp); // the first element that is NOT less than tmp (see "<"" of TimelineEvent)
+        auto e = events.lower_bound(tmp); // the first element that is NOT less than tmp (see "<" of TimelineEvent)
         if (e != events.begin()) {
             return *--e; // there is at least one element (see above) -> even if e is end(), we will get a valid event
-        } else if (allow_loop) { // e is the first element in the list -> the previous element is the last one
+        } else if (allow_loop) {
             return *--events.end();
         }
 
-        return TimelineEvent<PayloadData>(TIMELINE_ERR, -1.f); // no element NOT less than tmp AND no restart allowed
+        return TimelineEvent<PayloadData>(TIMELINE_ERR, -1.f);
     }
 
   private:
