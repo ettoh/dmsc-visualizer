92252a79bdd138abb5a45b4e432a5bf27522b566
Hendrik <83767323+mc-thulu@users.noreply.github.com>
2021-06-08 13:45:57 +0200

    Fixed issue with deleted function

--- src/visuals.cpp
+++ src/visuals.cpp
@@ -1,14 +1,5 @@
 #include "dmsc/visuals.hpp"
 #include "opengl_widgets.hpp"
-
-#include <glad/glad.h>
-#define GLFW_INCLUDE_NONE
-
-#include "imgui.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
-
-#include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -18,14 +9,8 @@ void visualizeInstance(const Instance& instance) { visualizeInstance(PhysicalIns
 
 void visualizeInstance(const PhysicalInstance& instance) {
     // TODO new thread?
-    OpenGLWidget gl = OpenGLWidget();
+    OpenGLWidget gl;
     gl.show(instance);
-}
-
-// ------------------------------------------------------------------------------------------------
-
-static void glfw_error_callback(int error, const char* description) {
-    fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
 } // namespace dmsc
